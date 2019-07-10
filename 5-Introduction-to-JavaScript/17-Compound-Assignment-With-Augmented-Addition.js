62cc135ff06f70fece58254dba7d1db73a81f23a
Nottakai <umachallapalli@gmail.com>
2019-07-10 09:57:41 -0600

    Lesson no 17

---
+++ 5-Introduction-to-JavaScript/17-Compound-Assignment-With-Augmented-Addition.js
@@ -0,0 +1,10 @@
+//This lesson shows how to add a number to a variable. There is a short cut to do this with +- symbols.
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only modify code below this line
+
+a += 12;
+b += 9;
+c += 7;
