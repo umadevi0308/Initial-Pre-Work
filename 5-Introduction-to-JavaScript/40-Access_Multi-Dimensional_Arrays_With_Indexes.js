9409b9b7523a5242b7e9930e17532321bea9b613
Nottakai <umachallapalli@gmail.com>
2019-07-10 16:55:44 -0600

    Lesson no 40

---
+++ 5-Introduction-to-JavaScript/40-Access_Multi-Dimensional_Arrays_With_Indexes.js
@@ -0,0 +1,6 @@
+// This lesson tells us how We can also use bracket notation to select an element in an array of arrays .
+// Setup
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+// Only change code below this line.
+var myData = myArray[2][1];
