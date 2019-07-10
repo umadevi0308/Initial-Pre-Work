1fa0eb56f61cf8b819b7ccce3a6851a09fd359ab
Nottakai <umachallapalli@gmail.com>
2019-07-10 15:55:57 -0600

    Lesson no 39

---
+++ 5-Introduction-to-JavaScript/39-Modify_Array_Data_With_Indexes.js
@@ -0,0 +1,12 @@
+//This lesson shows how  the entries of arrays are mutable and can be changed freely.
+// Example
+var ourArray = [18,64,99];
+ourArray[1] = 45; // ourArray now equals [18,45,99].
+
+// Setup
+var myArray = [18,64,99];
+
+// Only change code below this line.
+
+var myArray = [18,64,99];
+myArray[0] = 45; // myArray now equals [45,64,99];
