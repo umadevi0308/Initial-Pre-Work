9be63e5b9452ebc6438a50b3a7432f280693c40b
Nottakai <umachallapalli@gmail.com>
2019-07-10 14:28:01 -0600

    Lesson no 32

---
+++ 5-Introduction-to-JavaScript/32-Use_Bracket_Notation_to_Find_the_Nth_Character_in_a_String.js
@@ -0,0 +1,9 @@
+//We can use the bracket notation to find the Nth character in a string. This lesson shows how to find the third letter.// Example
+var firstName = "Ada";
+var secondLetterOfFirstName = firstName[1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var thirdLetterOfLastName = lastName[2];
