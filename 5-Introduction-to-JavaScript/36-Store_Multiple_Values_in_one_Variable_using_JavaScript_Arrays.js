a32756c31dbd52662fb1f9e29439fe92bba879bd
Nottakai <umachallapalli@gmail.com>
2019-07-10 15:26:45 -0600

    Lesson no 36

---
+++ 5-Introduction-to-JavaScript/36-Store_Multiple_Values_in_one_Variable_using_JavaScript_Arrays.js
@@ -0,0 +1,6 @@
+//We can store several datas in place.Array always start with the bracket and end with the bracket to show begening and ending.
+// Example
+var ourArray = ["John", 23];
+
+// Only change code below this line.
+var myArray = ["Uma", 12];
