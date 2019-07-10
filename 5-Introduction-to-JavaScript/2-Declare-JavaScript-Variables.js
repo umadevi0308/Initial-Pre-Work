7153e4eecddf22dde72914b6bf919a4c84207f96
Nottakai <umachallapalli@gmail.com>
2019-07-09 21:32:37 -0600

    lesson no 2

---
+++ 5-Introduction-to-JavaScript/2-Declare-JavaScript-Variables.js
@@ -0,0 +1,6 @@
+// This lesson shows us how to create variables in JS. Computer variables differ from mathematical variables in that they can store different values at different times. JS variables start with var in front.
+
+var ourName;
+
+// Declare myName below this line
+var myName;
