12ffab0733fed78fba4c284d16764b2725f87844
Nottakai <umachallapalli@gmail.com>
2019-07-10 14:23:31 -0600

    Lesson no 31

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,7 @@
+//Strings are immutable. It cannot be altered once its created.It does not mean that we cannot change at all. We cannot change a character but we can change the whole thing.
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World"; // Fix Me
