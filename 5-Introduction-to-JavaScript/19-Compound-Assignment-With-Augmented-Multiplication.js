cb277e659b32edf48bfb0d39d6264a9d2007b340
Nottakai <umachallapalli@gmail.com>
2019-07-10 10:02:55 -0600

    Lesson no 19

---
+++ 5-Introduction-to-JavaScript/19-Compound-Assignment-With-Augmented-Multiplication.js
@@ -0,0 +1,10 @@
+//This lesson shows how to multiply a number to a variable. There is a short cut to do this with *=symbols.
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only modify code below this line
+
+a *= 5;
+b *= 3;
+c *= 10;
