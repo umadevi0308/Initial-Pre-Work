46a6c4807098b46582b87a9ad2b06c64d05a2ef3
Nottakai <umachallapalli@gmail.com>
2019-07-10 09:51:25 -0600

    Lesson no 16

---
+++ 5-Introduction-to-JavaScript/16-Finding-a-Remainder-in-JavaScript.js
@@ -0,0 +1,5 @@
+// This lesson shows how the reminder operator looks like and it looks % in JS. It also determines the reminder number is evern or odd.
+// Only change code below this line
+
+var remainder;
+remainder = 11 % 3;
