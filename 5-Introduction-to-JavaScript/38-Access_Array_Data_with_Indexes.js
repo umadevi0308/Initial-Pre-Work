eb015f0a8c8bdf56bad567b4bd87c6094c64e250
Nottakai <umachallapalli@gmail.com>
2019-07-10 15:39:05 -0600

    Lesson no 38

---
+++ 5-Introduction-to-JavaScript/38-Access_Array_Data_with_Indexes.js
@@ -0,0 +1,11 @@
+// This lesson talks about how to do Bracket notation to find specific index in a string in an Array..
+// Example
+var ourArray = [50,60,70];
+var ourData = ourArray[0]; // equals 50
+
+// Setup
+var myArray = [50,60,70];
+
+// Only change code below this line.
+var myArray = [80,90,100];
+var myData = myArray[0]; // equals 80
