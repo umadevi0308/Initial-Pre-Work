f7e454d7bb4695c73caed7570286aa6b08751593
Nottakai <umachallapalli@gmail.com>
2019-07-10 09:27:23 -0600

    Lesson no 13

---
+++ 5-Introduction-to-JavaScript/13-Create-Decimal-Numbers-with-JavaScript.js
@@ -0,0 +1,5 @@
+//This lesson explains how to store Decimal numbers . Decimal numbers are sometimes referred to as floating point numbers or floats.
+var ourDecimal = 5.7;
+
+// Only change code below this line
+var myDecimal = 6.09;
