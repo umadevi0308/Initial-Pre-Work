3c904cc82245a767106b7494f6c1c38b56ecb176
Nottakai <umachallapalli@gmail.com>
2019-07-10 14:06:12 -0600

    Lesson no 28

---
+++ 5-Introduction-to-JavaScript/28-Appending_Variables_to_Strings.js
@@ -0,0 +1,11 @@
+//This lesson shows how to  build a string over multiple lines. we can also append variables to a string using the plus equals (+=) operator.
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "good";
+var myStr = "Learning to code is ";
+myStr +=someAdjective;
