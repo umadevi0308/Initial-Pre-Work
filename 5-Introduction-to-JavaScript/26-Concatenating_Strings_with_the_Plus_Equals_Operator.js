bbb2c782d21234e994b0563826711019a55ad272
Nottakai <umachallapalli@gmail.com>
2019-07-10 11:14:12 -0600

    Lesson no 26

---
+++ 5-Introduction-to-JavaScript/26-Concatenating_Strings_with_the_Plus_Equals_Operator.js
@@ -0,0 +1,9 @@
+//We can also use the += operator to concatenate a string onto the end of an existing string variable. This can be very helpful to break a long string over several lines.
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the first sentence. "
+myStr += "This is the second sentence."
