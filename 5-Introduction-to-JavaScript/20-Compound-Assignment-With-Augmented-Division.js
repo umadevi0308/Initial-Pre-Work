e436b757302445575d6171dbd31324c8e3f6f72d
Nottakai <umachallapalli@gmail.com>
2019-07-10 10:05:28 -0600

    Lesson no 20

---
+++ 5-Introduction-to-JavaScript/20-Compound-Assignment-With-Augmented-Division.js
@@ -0,0 +1,10 @@
+//This lesson shows how to Divide a number to a variable. There is a short cut to do this with /= symbols.
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only modify code below this line
+
+a /= 12;
+b /= 4;
+c /= 11;
