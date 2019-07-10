c5ad3d427761cb4e7e368b6baead37c22e3c04f1
Nottakai <umachallapalli@gmail.com>
2019-07-10 10:39:55 -0600

    Lesson no 22

---
+++ 5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
@@ -0,0 +1,3 @@
+/*This lesson shows how we can escape a quote from considering it as an end of string .
+Placing a backslash (\) in front of the quote helps JS to understand that this is not the end of the string. */
+var myStr = "I am a \"double quoted\" string inside \"double quotes\".";
