cb8067b625ea536173cabf04f5483b8eda64f14a
Nottakai <umachallapalli@gmail.com>
2019-07-10 15:31:46 -0600

    Lesson no 37

---
+++ 5-Introduction-to-JavaScript/37-Nest_one_Array_within_Another_Array.js
@@ -0,0 +1,5 @@
+//If an element of an Array is another Array , it is called nested Array.
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [["the pencil", 30], ["books", 400]];
