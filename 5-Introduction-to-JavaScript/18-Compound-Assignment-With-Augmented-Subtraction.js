1c00ba9a79e7b0f6f135541887af36c648e6ad00
Nottakai <umachallapalli@gmail.com>
2019-07-10 10:00:29 -0600

    Lesson no 18

---
+++ 5-Introduction-to-JavaScript/18-Compound-Assignment-With-Augmented-Subtraction.js
@@ -0,0 +1,10 @@
+//This lesson shows how to sustract a number to a variable. There is a short cut to do this with -= symbols.
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only modify code below this line
+
+a -= 6;
+b -= 15;
+c -= 1;
