176eba4a611fc6499ccbbf2b47259768114f6235
Nottakai <umachallapalli@gmail.com>
2019-07-10 10:14:49 -0600

    Lesson no 21

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,9 @@
+//This lesson shows whenever the characters are sarrounded by single or double quotation marks , they are called strings.
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+
+var myFirstName = "Umadevi";
+var myLastName = "Challapalli";
