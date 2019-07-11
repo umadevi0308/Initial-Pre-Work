8f502c6086e8cfc2dcea83cee05895db55972bc9
Nottakai <umachallapalli@gmail.com>
2019-07-11 08:57:25 -0600

    Lesson no 50

---
+++ 5-Introduction-to-JavaScript/50-Global_vs._Local_Scope_in_Functions.js
@@ -0,0 +1,14 @@
+//It is possible global and local scope with the same name.Local variable takes precidence of global variable in this case.
+// Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+
+  var outerWear = "sweater";
+
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
