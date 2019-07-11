fe620649ae19af10402b28b3b047d0e5567887c3
Nottakai <umachallapalli@gmail.com>
2019-07-11 11:16:14 -0600

    Lesson no 66

---
+++ 5-Introduction-to-JavaScript/66-Comparisons_with_the_Logical_And_Operator.js
@@ -0,0 +1,14 @@
+//This lesson is to check the two things are true at the same time.
+// Only change code below this line
+
+if (val <= 50 && val >= 25 )  {
+
+    return "Yes";
+  }
+
+// Only change code above this line
+return "No";
+}
+
+// Change this value to test
+testLogicalAnd(10);
