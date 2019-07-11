13578cc07f0c4a304aac4c428ff7325d2e6ba99f
Nottakai <umachallapalli@gmail.com>
2019-07-11 10:43:22 -0600

    Lesson no 58

---
+++ 5-Introduction-to-JavaScript/58-Comparison_with_the_Strict_Equality_Operator.js
@@ -0,0 +1,11 @@
+//This lesson is all about strict equality operator that is tripple equality operator.
+// Setup
+function testStrict(val) {
+  if (val === 7) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testStrict(10);
