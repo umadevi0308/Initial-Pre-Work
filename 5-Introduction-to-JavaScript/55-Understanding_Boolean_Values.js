6851507ee57c24007fcbc8d96bb2bf812eb0676c
Nottakai <umachallapalli@gmail.com>
2019-07-11 09:40:25 -0600

    Lesson no 55

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,8 @@
+//This lesson tells us about Boolean datas. It has only two values,  true or false. True switch is on and false switch is off.
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
