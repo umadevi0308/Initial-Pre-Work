2f1524b14ef33834a4e5877e6361567694b08761
Nottakai <umachallapalli@gmail.com>
2019-07-10 20:07:33 -0600

    Lesson no 46

---
+++ 5-Introduction-to-JavaScript/46-Write_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,13 @@
+//This lesson talk about Functions. Functions allow to create resuable codes in JS.
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+function reusableFunction() {
+    console.log("Hi World");
+    }
+    reusableFunction();
