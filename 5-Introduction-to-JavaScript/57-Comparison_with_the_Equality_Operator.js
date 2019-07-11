ef1b9c1320616f1cd255076dcb7d0cb57beb1aba
Nottakai <umachallapalli@gmail.com>
2019-07-11 10:39:51 -0600

    Lesson no 57

---
+++ 5-Introduction-to-JavaScript/57-Comparison_with_the_Equality_Operator.js
@@ -0,0 +1,12 @@
+/*This lesson talks about Equality Operators.  It is most commonly used in If statement.
+JS has equality operators that will return a bullion with true or false.*/
+// Setup
+function testEqual(val) {
+  if (val == 12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+console.log(testEqual(10));
