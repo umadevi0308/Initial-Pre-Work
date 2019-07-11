79782ded1922c7d8bb6cd76dc7aaac2192f80d48
Nottakai <umachallapalli@gmail.com>
2019-07-11 10:54:24 -0600

    Lesson no 61

---
+++ 5-Introduction-to-JavaScript/61-Comparison_with_the_Strict_Inequality_Operator.js
@@ -0,0 +1,16 @@
+//This lesson is about inequality of strict opertor which is opposite of strict equality operator.
+// Setup
+function testStrictNotEqual(val) {
+  // Only Change Code Below this Line
+
+  if (val !== 17) {
+
+  // Only Change Code Above this Line
+
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testStrictNotEqual(10);
