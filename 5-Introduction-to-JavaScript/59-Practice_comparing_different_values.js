384881840e6b904880980731aab9cc542399b323
Nottakai <umachallapalli@gmail.com>
2019-07-11 10:46:30 -0600

    Lesson no 59

---
+++ 5-Introduction-to-JavaScript/59-Practice_comparing_different_values.js
@@ -0,0 +1,11 @@
+// This lesson is all about the comparison and difference betwween strict equality and equality Operators.
+// Setup
+function compareEquality(a, b) {
+  if (a === b) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+compareEquality(10, "10");
