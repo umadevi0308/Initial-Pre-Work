acd7af4b236286eb03bd165d0009f2e3f2bc1e20
Nottakai <umachallapalli@gmail.com>
2019-07-11 10:51:17 -0600

    Lesson no 60

---
+++ 5-Introduction-to-JavaScript/60-Comparison_with_the_Inequality_Operator.js
@@ -0,0 +1,11 @@
+//this lesson talks about inequality operator which is quite opposite of equality operator.
+// Setup
+function testNotEqual(val) {
+  if (val != 99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testNotEqual(10);
