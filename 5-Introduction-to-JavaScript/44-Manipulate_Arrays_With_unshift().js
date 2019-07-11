e30613b3ab644d3258c23ac430b8e33a6531ace0
Nottakai <umachallapalli@gmail.com>
2019-07-10 19:49:24 -0600

    Lesson no 44

---
+++ 5-Introduction-to-JavaScript/44-Manipulate_Arrays_With_unshift().js
@@ -0,0 +1,14 @@
+//This lesson shows us how to unshift the elements to the beginning of an array.
+// Example
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.shift(); // ourArray now equals ["J", "cat"]
+ourArray.unshift("Happy");
+// ourArray now equals ["Happy", "J", "cat"]
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+// Only change code below this line.
+
+myArray.unshift(["Paul",35]);
