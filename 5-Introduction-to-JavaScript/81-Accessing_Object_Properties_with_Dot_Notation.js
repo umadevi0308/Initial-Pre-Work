46b6a9f0e2dd20fa7d7d66d7aebd320095dfeae7
Nottakai <umachallapalli@gmail.com>
2019-07-12 10:04:06 -0600

    Lesson no 81

---
+++ 5-Introduction-to-JavaScript/81-Accessing_Object_Properties_with_Dot_Notation.js
@@ -0,0 +1,12 @@
+//This lesson tells us how to access the properties with Dot Notataion.There are two main ways to access the properties on an object.
+// Setup
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+
+var hatValue = testObj.hat;      // Change this line
+var shirtValue = testObj.shirt;    // Change this line
