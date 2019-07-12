c8a0a92da599b97a2d7a94e6cb0f28737bf6cbe2
Nottakai <umachallapalli@gmail.com>
2019-07-12 09:58:52 -0600

    Lesson no 80

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,15 @@
+//This lesson is about Objects. Objects are similar to arrays instead of using indexes to access data we use properties.
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+// Only change code below this line.
+  var myDog = {
+  "name": "Jimmy",
+  "legs": 2,
+  "tails": 2,
+  "friends": ["something"]
+};
