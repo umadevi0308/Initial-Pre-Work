21ff6168e9b98d9231fdbbfaacd6eababd61c26f
Nottakai <umachallapalli@gmail.com>
2019-07-12 10:08:42 -0600

    Lesson no 82

---
+++ 5-Introduction-to-JavaScript/82-Accessing_Object_Properties_with_Bracket_Notation.js
@@ -0,0 +1,12 @@
+//his lesson tells us how to access the properties with Bracket Notataion.
+// Setup
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+
+var entreeValue = testObj["an entree"];   // Change this line
+var drinkValue = testObj["the drink"];    // Change this line
