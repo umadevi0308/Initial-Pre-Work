c5f95a3e7b2d9c9ab6a64f5f32442fc707aede05
Nottakai <umachallapalli@gmail.com>
2019-07-12 15:31:41 -0600

    Lesson no 107

---
+++ 5-Introduction-to-JavaScript/107-Use_Multiple_Conditional_(Ternary)_Operators.js
@@ -0,0 +1,6 @@
+// We can Nest the Ternary Operators by chaining them together to check for multiple conditions.
+function checkSign(num) {
+ return num > 0 ? "positive" : num < 0 ? "negative"  : "zero"
+}
+
+checkSign(10);
