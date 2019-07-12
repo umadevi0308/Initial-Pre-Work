75967a33e1c089d1a963409a484fc6cceaac4944
Nottakai <umachallapalli@gmail.com>
2019-07-12 15:13:40 -0600

    Lesson no 106

---
+++ 5-Introduction-to-JavaScript/106-Use_the_Conditional_(Ternary)_Operator.js
@@ -0,0 +1,6 @@
+//This lesson is about the Ternary Operator.The conditional operator, also called the ternary operator, can be used as a one line if-else expression.
+function checkEqual(a, b) {
+  return a === b ? true : false;
+}
+
+checkEqual(1, 2);
