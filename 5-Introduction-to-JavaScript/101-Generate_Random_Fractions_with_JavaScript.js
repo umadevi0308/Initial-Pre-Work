99edcc337ce8f98d2be28d8f428ac9cde90ba15e
Nottakai <umachallapalli@gmail.com>
2019-07-12 14:36:09 -0600

    Lesson no 101

---
+++ 5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
@@ -0,0 +1,9 @@
+//There is a  way to  create  a random  decimal number in JS with Math.random function.
+function randomFraction() {
+
+  // Only change code below this line.
+
+  return Math.random();
+
+  // Only change code above this line.
+}
