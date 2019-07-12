aec9007bacae86af232ed15605191ae580748610
Nottakai <umachallapalli@gmail.com>
2019-07-12 15:42:28 -0600

    Lesson no 25

--- 5-Introduction-to-JavaScript/25-Concatenating_Strings_with_Plus_Operator.js
+++ 5-Introduction-to-JavaScript/25-Concatenating_Strings_with_Plus_Operator.js
@@ -4,4 +4,4 @@ var ourStr = "I come first. " + "I come second.";
 
 // Only change code below this line
 
-var myStr = "This is the start. " + "This is the end.";
+var myStr = "This is the start. " + "This is the end."
