1fcc1ce75383008c5f35e65febee05d5d3c3d261
Nottakai <umachallapalli@gmail.com>
2019-07-12 14:44:50 -0600

    Lesson no 102

---
+++ 5-Introduction-to-JavaScript/102-Generate_Random_Whole_Numbers_with_JavaScript.js
@@ -0,0 +1,8 @@
+//We can create random Whole numbers instead of decimal numbers. Math.floor() does that.
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+function randomWholeNum() {
+
+  // Only change code below this line.
+
+  return Math.floor(Math.random() * 10);
