3b1e0610d78c6118032101c1dfd0e0ee27196cd4
Nottakai <umachallapalli@gmail.com>
2019-07-12 12:32:37 -0600

    Lesson no 94

---
+++ 5-Introduction-to-JavaScript/94-Iterate_with_JavaScript_For_Loops.js
@@ -0,0 +1,16 @@
+//This lesson is about for loop. It is very common loop.
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+var myArray = [];
+for (var i = 1; i < 6; i++) {
+  myArray.push(i);
+}
