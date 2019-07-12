7234404ee100acaefafb370fc3dc09595d926752
Nottakai <umachallapalli@gmail.com>
2019-07-12 10:41:14 -0600

    Lesson no 87

---
+++ 5-Introduction-to-JavaScript/87-Using_Objects_for_Lookups.js
@@ -0,0 +1,23 @@
+//Objects can be thought of as a dictionary.  We can use an object to "lookup" values rather than a switch statement or an if/else chain.
+// Setup
+function phoneticLookup(val) {
+  var result = "";
+
+  // Only change code below this line
+ var lookup = {
+   "alpha": "Adams",
+   "bravo": "Boston",
+   "charlie": "Chicago",
+   "delta": "Denver",
+   "echo": "Easy",
+   "foxtrot": "Frank"
+ };
+ result = lookup[val];
+
+
+  // Only change code above this line
+  return result;
+}
+
+// Change this value to test
+phoneticLookup("charlie");
