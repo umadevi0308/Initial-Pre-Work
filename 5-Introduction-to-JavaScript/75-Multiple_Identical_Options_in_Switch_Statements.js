871cb3ce183308478eff9fe9292b575df0eb4759
Nottakai <umachallapalli@gmail.com>
2019-07-11 20:06:08 -0600

    Lesson no 75

--- 5-Introduction-to-JavaScript/75-Multiple_Identical_Options_in_Switch_Statements.js
+++ 5-Introduction-to-JavaScript/75-Multiple_Identical_Options_in_Switch_Statements.js
@@ -1,1 +1,25 @@
-
+//Where muliple inputs gives same output, switch statement is used.
+function sequentialSizes(val) {
+  var answer = "";
+  // Only change code below this line
+    switch(val) {
+  case 1:
+  case 2:
+  case 3:
+   answer = "Low";
+   break;
+  case 4:
+  case 5:
+  case 6:
+   answer = "Mid";
+   break;
+  case 7:
+  case 8:
+  case 9:
+   answer = "High";
+   break;
+ }
+  return answer;
+}
+// Change this value to test
+sequentialSizes(1);
