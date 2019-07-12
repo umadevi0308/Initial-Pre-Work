b9b6a76f265dd2b23b1c79a5d305dd6b91397ccb
Nottakai <umachallapalli@gmail.com>
2019-07-12 09:32:19 -0600

    Lesson no 77

---
+++ 5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
@@ -0,0 +1,8 @@
+// Thid lesson tells how we may recall from Comparison with the Equality Operator that all comparison operators return a boolean true or false value.
+function isLess(a, b) {
+  // Fix this code
+ return a < b ;
+    }
+
+// Change these values to test
+isLess(10, 15);
