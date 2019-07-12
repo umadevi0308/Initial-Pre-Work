a5f82c02f719408d1fbd78a50034841f7625d775
Nottakai <umachallapalli@gmail.com>
2019-07-12 11:40:35 -0600

    Lesson no 89

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,25 @@
+//JS object is another way to store flexible data. we can store manythings like numbers, strings, arrays etc.
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+   },
+   // Add record here
+   {
+
+    "artist": "AR",
+    "title": "Jai",
+    "release_year": 2010,
+    "formats": [
+      "video",
+      "CD"
+      ]
+   }
+]
