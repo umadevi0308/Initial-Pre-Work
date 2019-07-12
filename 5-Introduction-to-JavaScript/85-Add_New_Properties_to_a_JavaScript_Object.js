c77cc213ae746ba5be40ffc4f0cd0d3bb4a7c3c6
Nottakai <umachallapalli@gmail.com>
2019-07-12 10:22:39 -0600

    Lesson no 85

---
+++ 5-Introduction-to-JavaScript/85-Add_New_Properties_to_a_JavaScript_Object.js
@@ -0,0 +1,21 @@
+//This lesson shows how we add new properties using dot or bracket notation.
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.bark = "bow-wow";
+
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line.
+myDog.bark = "Woof!"
