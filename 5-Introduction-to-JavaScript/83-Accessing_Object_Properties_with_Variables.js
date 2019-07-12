ea05f8e6e1bac0bcc37f288633e91d460656e37e
Nottakai <umachallapalli@gmail.com>
2019-07-12 10:14:28 -0600

    Lesson no 83

---
+++ 5-Introduction-to-JavaScript/83-Accessing_Object_Properties_with_Variables.js
@@ -0,0 +1,12 @@
+// Bracket notation can also look up  the object  properties using variables.
+// Setup
+var testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line;
+
+var playerNumber = 16;       // Change this Line
+var player = testObj[playerNumber];   // Change this Line
