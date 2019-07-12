8397e22370e90a2d0b888bd0187fa6fde1df5cf9
Nottakai <umachallapalli@gmail.com>
2019-07-12 12:37:13 -0600

    Lesson no 95

---
+++ 5-Introduction-to-JavaScript/95-Iterate_Odd_Numbers_With_a_For_Loop.js
@@ -0,0 +1,16 @@
+// This lesson tells us about For loop. For loop don't have to iterate one at a time. By changing our final-expression, we can count by even numbers.
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 10; i += 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+var myArray = [];
+for (var i = 1; i < 10; i +=2){
+  myArray.push(i);
+}
