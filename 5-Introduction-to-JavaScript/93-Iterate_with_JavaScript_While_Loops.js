0e6f83dd60236a9e0fa1d62a41cdc8cbd0d60046
Nottakai <umachallapalli@gmail.com>
2019-07-12 12:27:12 -0600

    Lesson no 93

---
+++ 5-Introduction-to-JavaScript/93-Iterate_with_JavaScript_While_Loops.js
@@ -0,0 +1,10 @@
+//This lesson shows how to run the same code multiple times by using the loop.
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+var i = 0;
+while(i < 5) {
+    myArray.push(i);
+    i++;
+  }
