a728044314039e3e2888d7b9e6ddd72d1996c863
Nottakai <umachallapalli@gmail.com>
2019-07-12 20:03:02 -0600

    5th lesson

---
+++ 6-Introduction-to-the-ES6-Challenges/5-Prevent-Object-Mutation.js
@@ -0,0 +1,19 @@
+/* const declaration alone doesn't really protect the  data from mutation. 
+JavaScript provides a function Object.freeze to prevent data mutation.*/
+function freezeObj() {
+  "use strict";
+  const MATH_CONSTANTS = {
+    PI: 3.14
+  };
+  // change code below this line
+Object.freeze (MATH_CONSTANTS);
+
+  // change code above this line
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch( ex ) {
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
