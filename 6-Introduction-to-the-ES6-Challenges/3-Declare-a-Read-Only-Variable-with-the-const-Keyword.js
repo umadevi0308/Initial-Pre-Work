2c92230e9bd92fb7bed767f929de260f28cc1603
Nottakai <umachallapalli@gmail.com>
2019-07-12 19:29:45 -0600

    3rd lesson

---
+++ 6-Introduction-to-the-ES6-Challenges/3-Declare-a-Read-Only-Variable-with-the-const-Keyword.js
@@ -0,0 +1,15 @@
+//This lesson is about const and it is another variable. It has all the features of let but it has read only.
+function printManyTimes(str) {
+  "use strict";
+
+  // change code below this line
+
+ const SENTENCE = str + " is cool!";
+  for(let i = 0; i < str.length; i+=2) {
+    console.log(SENTENCE);
+  }
+
+  // change code above this line
+
+}
+printManyTimes("freeCodeCamp");
