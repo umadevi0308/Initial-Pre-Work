dff2fb1385a936ade41c2b6720854d3bde398e85
Nottakai <umachallapalli@gmail.com>
2019-07-12 20:30:08 -0600

    7th lesson

---
+++ 6-Introduction-to-the-ES6-Challenges/7-Write-Arrow-Functions-with-Parameters.js
@@ -0,0 +1,4 @@
+//this lesson tells us Just like normal functions we can pass arguments to arrow functions.
+const myConcat = (arr1, arr2) => arr1.concat(arr2);
+// test your code
+console.log(myConcat([1, 2], [3, 4, 5]));
