e9b752115d4c673d93d45beed641717a6cd2dace
Nottakai <umachallapalli@gmail.com>
2019-07-12 21:00:35 -0600

    9th lesson

---
+++ 6-Introduction-to-the-ES6-Challenges/9-Set-Default-Parameters-for-Your-Functions.js
@@ -0,0 +1,9 @@
+//In order to help us create more flexible functions, ES6 introduces default parameters for functions.
+const increment = (function() {
+  "use strict";
+  return function increment(number, value = 1) {
+    return number + value;
+  };
+})();
+console.log(increment(5, 2)); // returns 7
+console.log(increment(5)); // returns 6
