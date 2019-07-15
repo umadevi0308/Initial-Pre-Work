890770a0da48a38ecce931cdf7fe7cf76b0971c7
Nottakai <umachallapalli@gmail.com>
2019-07-14 21:27:44 -0600

    17th lesson

---
+++ 6-Introduction-to-the-ES6-Challenges/17-Create-Strings-using-Template-Literals.js
@@ -0,0 +1,23 @@
+//This lesson tells us how to create a string using Template Literals.This is a special type of string that makes creating complex strings easier.
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["id-blacklist", "no-dup-keys"]
+};
+function makeList(arr) {
+  "use strict";
+
+  // change code below this line
+ const resultDisplayArray = arr.map(item => `<li class="text-warning">${item}</li>`);
+
+  // change code above this line
+
+  return resultDisplayArray;
+}
+/**
+ * makeList(result.failure) should return:
+ * [ `<li class="text-warning">no-var</li>`,
+ *   `<li class="text-warning">var-on-top</li>`,
+ *   `<li class="text-warning">linebreak</li>` ]
+ **/
+const resultDisplayArray = makeList(result.failure);
