c2a16cac18089fbf9013c869b7fcb0668bda29f1
Nottakai <umachallapalli@gmail.com>
2019-07-14 20:05:27 -0600

    22nd lesson

---
+++ 6-Introduction-to-the-ES6-Challenges/22-Understand-the-Differences-Between-import-and-require.js
@@ -0,0 +1,3 @@
+//This lesson tells about Import and Export. We can export a code in one file and import in another file. It allows only certain variables.
+import { capitalizeString } from "string_functions"
+const cap = capitalizeString("hello!");
