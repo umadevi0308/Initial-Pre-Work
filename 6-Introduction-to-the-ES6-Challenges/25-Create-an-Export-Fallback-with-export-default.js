76eb5c16a910c89902f8062f8aa0184624fa74ba
Nottakai <umachallapalli@gmail.com>
2019-07-14 20:24:04 -0600

    25th lesson

---
+++ 6-Introduction-to-the-ES6-Challenges/25-Create-an-Export-Fallback-with-export-default.js
@@ -0,0 +1,3 @@
+//Named exports are also called export default. This is a fallback export. It is used only if you want to export onething.
+"use strict";
+export default function subtract(x,y) {return x - y;}
