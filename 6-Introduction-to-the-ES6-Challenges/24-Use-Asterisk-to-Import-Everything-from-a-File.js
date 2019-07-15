2ab1a07567d596458c47d34bac7d9b6ed4669b5d
Nottakai <umachallapalli@gmail.com>
2019-07-14 20:19:14 -0600

    24th lesson

---
+++ 6-Introduction-to-the-ES6-Challenges/24-Use-Asterisk-to-Import-Everything-from-a-File.js
@@ -0,0 +1,2 @@
+//If a file exports multiple different things we can import each thing individully or everything. We can import Everything by using Asterisk.
+import * as capitalizeStrings from "capitalize_strings";
